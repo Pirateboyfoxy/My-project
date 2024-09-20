b251297e3ef9c302d0e6f196f5e530eae1cfba25
Pirateboyfoxy <146957835+Pirateboyfoxy@users.noreply.github.com>
2024-09-20 12:38:21 -0400

    plane fix

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(65.6f, 4.1f, 0.6f);
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
